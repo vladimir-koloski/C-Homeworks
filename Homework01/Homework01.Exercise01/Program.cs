877ac1038a70ef1e1ea15cf6f84974d4149769d6
vladimir-koloski <v.koloski1@gmail.com>
2020-03-05 19:30:08 +0100

    change

--- Homework01/Homework01.Exercise01/Program.cs
+++ Homework01/Homework01.Exercise01/Program.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Threading;
 
 namespace Homework01.Exercise01
+
 {
     class Program
     {
-        static void Main(string[] args)
+        public class ThreadExample
         {
-            Console.WriteLine("Traffic light...");
-            Console.BackgroundColor
-            = ConsoleColor.Red;
-            Console.WriteLine("Red Light - STOP!");
-            Console.BackgroundColor
-            = ConsoleColor.Yellow;
-            Console.WriteLine("Yellow Light - PREPARE.");
-            Console.BackgroundColor
-            = ConsoleColor.Green;
-            Console.WriteLine("Green Light - GO!");
+            
+            public static void ThreadProc()
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("ThreadProc: {0}", i);
+                    Thread.Sleep(0);
+                }
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Traffic light...");
+                Console.BackgroundColor
+                = ConsoleColor.Red;
+                Console.WriteLine("Red Light - STOP!");
+                Console.BackgroundColor
+                = ConsoleColor.Yellow;
+                Console.WriteLine("Yellow Light - PREPARE.");
+                Console.BackgroundColor
+                = ConsoleColor.Green;
+                Console.WriteLine("Green Light - GO!");
 
-            Console.ReadLine();
+                Console.ReadLine();
 
-          
+
+            }
         }
     }
 }
