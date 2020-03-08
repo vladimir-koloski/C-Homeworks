e144f00377510f9f12e637977799ffbfc4c7cb1f
vladimir-koloski <v.koloski1@gmail.com>
2020-03-08 13:42:44 +0100

    change

--- Homework01/Homework02/Program.cs
+++ Homework01/Homework02/Program.cs
@@ -7,62 +7,124 @@ namespace Homework02
         static void Main(string[] args)
         {
 
-            #region Task1
+            //#region Task1
 
+            //Console.WriteLine("Enter the first number");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Operation");
+            //char operation;
+            //operation = Convert.ToChar(Console.ReadLine());
+
+            //if (operation == '+')
+            //    Console.WriteLine("The result is", a + b);
+            //else if (operation == '-')
+            //    Console.WriteLine("The result is", a - b);
+            //else if (operation == '*')
+            //    Console.WriteLine("The result is", a * b);
+            //else if (operation == '/')
+            //    Console.WriteLine("The result is", a / b);
+            //else
+            //    Console.WriteLine("Wrong Character");
+
+
+            //#endregion
+            //#region Task2
+
+            //Console.WriteLine("Enter the first number");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number");
+            //int d = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the third number");
+            //int e = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the four number");
+            //int f = int.Parse(Console.ReadLine());
+            //int result = (c + d + e + f) / 4;
+
+            //Console.WriteLine("The Average of " + c + d + e +  " and " + f + " is " + result);
+            //#endregion
+
+            //#region Task3
+            //Console.WriteLine("Enter the first number");
+            //int firstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the second number");
+            //int secondNumber = int.Parse(Console.ReadLine());
+
+            //firstNumber = firstNumber + secondNumber;
+            //secondNumber = firstNumber - secondNumber;
+            //firstNumber = firstNumber - secondNumber;
+
+            //Console.WriteLine("After swaping");
+            //Console.WriteLine("First Number:", firstNumber);
+            //Console.WriteLine("Second Number:", secondNumber);
+
+            //#endregion
+
+            //#region Exercise07
+
+            //Console.WriteLine("Enter a number from 1 to 3");
+            //int day = int.Parse(Console.ReadLine());
+
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("You got a new car");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("You got a new plane");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("You got a new bike");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Error, Put the corect number");
+            //        break;
+            //}
+            //#endregion
+
+            #region Exercise06
+
+            int firstNum;
+            int secondNum;
             Console.WriteLine("Enter the first number");
-            int a = int.Parse(Console.ReadLine());
+            firstNum = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the second number");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the Operation");
-            char operation;
-            operation = Convert.ToChar(Console.ReadLine());
+            secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(checkLarger(firstNum, secondNum));
+            int result = int.Parse(Console.ReadLine());
+            Console.WriteLine(checkEven(result));
 
-            if (operation == '+')
-                Console.WriteLine("The result is", a + b);
-            else if (operation == '-')
-                Console.WriteLine("The result is", a - b);
-            else if (operation == '*')
-                Console.WriteLine("The result is", a * b);
-            else if (operation == '/')
-                Console.WriteLine("The result is", a / b);
-            else
-                Console.WriteLine("Wrong Character");
+            string checkEven(int result)
+            {
+                if (result % 2 != 0)
+                {
+                    return Console.WriteLine("The number is odd");
+                }
+                else
+                {
+                    return Console.WriteLine("The number is even");
+                }
+            }
+            
+            int checkLarger(int firstNum, int secondNum)
+            {
+                if (firstNum > secondNum)
+                {
+                     return firstNum;
+                }
+                else
+                {
+                    return secondNum;
+                } 
+            }
+            
 
 
-            #endregion
-            #region Task2
 
-            Console.WriteLine("Enter the first number");
-            int c = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            int d = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the third number");
-            int e = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the four number");
-            int f = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("The Average of", c, d, e, "and", f, "is", (c + d + e + f) / 4);
-            #endregion
-
-            #region Task3
-            Console.WriteLine("Enter the first number");
-            int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
-
-            firstNumber = firstNumber + secondNumber;
-            secondNumber = firstNumber - secondNumber;
-            firstNumber = firstNumber - secondNumber;
-
-            Console.WriteLine("After swaping");
-            Console.WriteLine("First Number:", firstNumber);
-            Console.WriteLine("Second Number:", secondNumber);
-
-            #endregion
+            
             Console.ReadLine();
-
-
+            #endregion
         }
-
-    }
+    } 
 }
