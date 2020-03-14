d910ecf379e439ad84aa7a19d3bceb1dac37c48d
vladimir-koloski <v.koloski1@gmail.com>
2020-03-14 21:50:25 +0100

    commit

---
+++ Homework01/Exercises/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            #region Task1
+
+            //Console.WriteLine("Enter a number higher than 3");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 3)
+            //{
+            //    for (int i = 1; i <= number; i++)
+            //    {
+            //        if (i % 3 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            #endregion
+            #region Task2
+
+            //Console.WriteLine("Enter a number higher than 5");
+            //int number1 = int.Parse(Console.ReadLine());
+            //if (number1 > 5)
+            //{
+            //    for (int i = 1; i <= number1; i++)
+            //    {
+            //        if (i % 2 == 0 && i % 3 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            #endregion
+            #region Task3
+
+            //Console.WriteLine("Enter a number");
+            //int number2 = int.Parse(Console.ReadLine());
+            //    int product = 1;
+            //    while (number2 != 0)
+            //    {
+            //        int d = number2 % 10;
+            //        number2 = number2 / 10;
+            //        product *= d;
+
+            //    }
+            //    Console.WriteLine(product);
+            #endregion
+            #region Task4
+
+            //Console.WriteLine("Enter a number");
+            //int number3 = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for(int i = 1; i <= number3; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task5
+
+            //Console.WriteLine("Enter a number");
+            //int number4 = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= number4; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task6
+
+            //Console.WriteLine("Enter a number");
+            //int number5 = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= number5; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task7
+
+            //string[] arrayOfLetters = new string[] {"a","b", "c", "d"};
+            //Array.Reverse(arrayOfLetters);
+            //foreach (string element in arrayOfLetters)
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            #endregion
+            #region 9
+
+            //for (int i = 1; i < 1001; i++)
+            //{
+            //    int sum = 0;
+            //    while (i != 0)
+            //    {
+            //        int d = i % 10;
+            //        i = i / 10;
+            //        sum += d;
+            //    }
+            //    if (i % sum == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region Task10
+
+
+            //for (int i = 1; i < 1001; i++)
+            //{
+            //    int product = 1;
+            //    while (i != 0)
+            //    {
+            //        int d = i % 10;
+            //        i = i / 10;
+            //        product *= d;
+            //    }
+            //    if(i % product == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region Task11
+
+
+            //for(int i = 1; i <= 100; i++)
+            //{
+            //    if(i % 3 == 0)
+            //    {
+            //        string d = i.ToString("Fizz");
+            //        Console.WriteLine(d);
+            //    }
+            //    if (i % 5 == 0)
+            //    {
+            //        string d = i.ToString("Buzz");
+            //        Console.WriteLine(d);
+            //    }
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        string d = i.ToString("FizzBuzz");
+            //        Console.WriteLine(d);
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
