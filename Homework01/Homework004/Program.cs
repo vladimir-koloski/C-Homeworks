18691f6e6ed49d9be3c8b7e071be52013c8347ad
vladimir-koloski <v.koloski1@gmail.com>
2020-03-24 23:43:26 +0100

    update

--- Homework01/Homework004/Program.cs
+++ Homework01/Homework004/Program.cs
@@ -27,26 +27,51 @@ namespace Homework04
 
         #region Task1
         /*Create a function to say whether a string is symmetric(a palindrome).*/
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(IsPalindrome("racecar"));
+        //    Console.WriteLine(IsPalindrome("car"));
+        //    Console.ReadLine();
+        //}
+
+        //public static bool IsPalindrome(string myString)
+        //{
+
+        //    int length = myString.Length;
+        //    for (int i = 0; i < length / 2; i++)
+        //    {
+        //        if (myString[i] != myString[length - i - 1])
+        //            return false;
+        //    }
+        //    return true;
+        //}
+
+
+
+        #endregion
+        #region Task02
+        //Create a function which returns the greatest value stored in an array of real numbers which is specified as parameter.
         static void Main(string[] args)
         {
-            Console.WriteLine(IsPalindrome("racecar"));
-            Console.WriteLine(IsPalindrome("car"));
+            
+            Console.WriteLine(FindMaxNum (new int[] {2, 6, 7, 13, 23, 12 }));
             Console.ReadLine();
         }
 
-        public static bool IsPalindrome(string myString)
+        public static int FindMaxNum(int[] realNumbers)
         {
-
-            int length = myString.Length;
-            for (int i = 0; i < length / 2; i++)
+            int [] realNumbers = new int[]{ };
+            int maxNum = 0;
+            for (int i = 0; i < realNumbers.Length; i++)
             {
-                if (myString[i] != myString[length - i - 1])
-                    return false;
+                if (realNumbers[i] > maxNum)
+                {
+                    maxNum = i;
+                }
+                    
             }
-            return true;
+            
         }
-
-
 
         #endregion
 
