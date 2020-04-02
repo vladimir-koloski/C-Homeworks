7d63fbed198a21883340dbe4187194dde0dde8fe
vladimir-koloski <v.koloski1@gmail.com>
2020-04-02 19:23:40 +0200

    change

---
+++ Homework01/SEDC.Class06.Exercises/SEDC.Class06.Exercises.Bussiness/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Class06.Exercises.Bussiness.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime CreateOn{ get; set; }
+
+        public User(string email, string password, string firstName, string lastName, DateTime dateOfBirth)
+        {
+            Id = GenerateId();
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            DateofBirth = dateOfBirth;
+            CreateOn = DateTime.Now;
+
+        }
+
+        public string GetInfo()
+        {
+            return string.Format("Name: {0} Date of birth:{1}", GetFullName(), DateofBirth.Date); 
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName}{LastName}";
+        }
+
+        private int GenerateId()
+        {
+            Random rand = new Random();
+            return rand.Next(0, 10_000_000);
+        }
+
+        
+
+
+    }
+}
---
+++ SEDC.Class06.Exercises/SEDC.Class06.Exercises.App/Program.cs
@@ -0,0 +1,80 @@
+﻿using SEDC.Class06.Exercises.Bussiness.Models;
+using SEDC.Class06.Exercises.Bussiness.Services;
+using System;
+
+namespace SEDC.Class06.Exercises.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            User [] users = new User[] { };
+            
+            while(true)
+            {
+                Console.WriteLine("Enter 'Register' if you want to make new profile or 'Login' if you are allready registred");
+                if (Console.ReadLine() == "Register")
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Enter email:");
+                        string inputEmail = Console.ReadLine();
+                        var validationService = new ValidationService();
+                        if (!validationService.ValidateEmail(inputEmail))
+                        {
+                            Console.WriteLine("Enter Valid email");
+                        }
+
+                        Console.WriteLine("Enter password:");
+                        string inputPasword = Console.ReadLine();
+                        if (!validationService.ValidatePassword(inputPasword))
+                        {
+                            Console.WriteLine("Password is not valid plese enter min and special caaracter");
+                        }
+
+                        Console.WriteLine("Enter First Name:");
+                        string inputFirstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name:");
+                        string inputLastName = Console.ReadLine();
+                        Console.WriteLine("Enter Date of Birth:");
+                        DateTime inpututDate = Convert.ToDateTime(Console.ReadLine());
+
+
+
+                        User user = new User(inputEmail, inputPasword, inputFirstName, inputLastName, inpututDate);
+                        Array.Resize(ref users, users.Length + 1);
+                        users[users.Length - 1] = user;
+
+                        Console.WriteLine("Do you want to register another user? Enter yes or no");
+                        if (Console.ReadLine() == "yes")
+                        {
+                            continue;
+                        }
+                        if (Console.ReadLine() == "no")
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (Console.ReadLine() == "Login")
+                {
+                    Console.WriteLine("Enter email:");
+                    string inputEmail = Console.ReadLine();
+                    Console.WriteLine("Enter password:");
+                    string inputPasword = Console.ReadLine();
+                    foreach (var user in users)
+                    {
+                        if (inputEmail == user.Email && inputPasword == user.Password)
+                            Console.WriteLine(user.GetInfo());
+                    }
+                    break;
+                }
+                Console.ReadLine();
+            }                      
+                
+            
+        }
+            
+        
+    }
+}
