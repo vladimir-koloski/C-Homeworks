6ddfc974b7075759729dbd4609451dc7266a5999
vladimir-koloski <v.koloski1@gmail.com>
2020-04-04 17:22:23 +0200

    change

--- Homework01/SEDC.Class06.Exercises/SEDC.Class06.Exercises.App/Program.cs
+++ Homework01/SEDC.Class06.Exercises/SEDC.Class06.Exercises.App/Program.cs
@@ -8,31 +8,27 @@ namespace SEDC.Class06.Exercises.App
     {
         static void Main(string[] args)
         {
-            User[] users = new User[] { };
-            bool login = true;
-            bool success = true;
-            while (login)
+            
+            
+            Console.ReadLine();
+        }
+
+        public static bool Register()
+        {
+            bool registration = true;
+            while (registration)
             {
                 Console.WriteLine("Enter 'Register' if you want to make new profile or 'Login' if you are allready registred");
                 if (Console.ReadLine() == "Register")
                 {
-                    while (success)
+                    while (registration)
                     {
                         Console.WriteLine("Enter email:");
                         string inputEmail = Console.ReadLine();
-                        var validationService = new ValidationService();
-                        if (!validationService.ValidateEmail(inputEmail))
-                        {
-                            Console.WriteLine("Enter Valid email");
-                        }
-
+                        EnterEmail(inputEmail);
                         Console.WriteLine("Enter password:");
                         string inputPasword = Console.ReadLine();
-                        if (!validationService.ValidatePassword(inputPasword))
-                        {
-                            Console.WriteLine("Password is not valid please enter number and one Upper Case letter in password");
-                        }
-
+                        EnterPassword(inputPasword);
                         Console.WriteLine("Enter First Name:");
                         string inputFirstName = Console.ReadLine();
                         Console.WriteLine("Enter Last Name:");
@@ -41,22 +37,24 @@ namespace SEDC.Class06.Exercises.App
                         DateTime inpututDate = Convert.ToDateTime(Console.ReadLine());
 
 
-
+                        User[] users = new User[] { };
                         User user = new User(inputEmail, inputPasword, inputFirstName, inputLastName, inpututDate);
                         Array.Resize(ref users, users.Length + 1);
                         users[users.Length - 1] = user;
-
+                        foreach (var user1 in users)
+                        {
+                            if (inputEmail == user1.Email && inputPasword == user1.Password)
+                                Console.WriteLine(user.GetInfo());
+                        }
                         Console.WriteLine("Do you want to register another user? Enter yes or no");
-                        if (Console.ReadLine() == "no")
-                        {
-                            success = false;
-                        }
-                        if (Console.ReadLine() == "yes")
-                        {
-                            continue;
-                        }
+                        string action = Console.ReadLine();
+                        AnotherRegistration();
+                    }
+                                        
+                                                       
                     }
                 }
+    
                 if (Console.ReadLine() == "Login")
                 {
                     Console.WriteLine("Enter email:");
@@ -68,15 +66,44 @@ namespace SEDC.Class06.Exercises.App
                         if (inputEmail == user.Email && inputPasword == user.Password)
                             Console.WriteLine(user.GetInfo());
                     }
-                    login = false;
-
-                }
-                Console.ReadLine();
+                registration = false;
+                }           
+         
+        }
+        static string EnterEmail(string email)
+        {
+            var validationService = new ValidationService();
+            if (!validationService.ValidateEmail(email))
+            {
+                Console.WriteLine("Enter Valid email");
             }
-
-
+            return email;
+        }
+        static string EnterPassword(string password)
+        {
+            var validationService = new ValidationService();
+            if (!validationService.ValidatePassword(password))
+            {
+                Console.WriteLine("Password is not valid please enter number and one Upper Case letter in password");
+            }
+            return password;
         }
 
-
+        static bool AnotherRegistration(string action)
+        {
+            if (action == "no")
+            {
+                registration = false;
+            }
+            if (action == "yes")
+            {
+                Register();
+            }
+        }
     }
+
+
 }
+
+
+ 
