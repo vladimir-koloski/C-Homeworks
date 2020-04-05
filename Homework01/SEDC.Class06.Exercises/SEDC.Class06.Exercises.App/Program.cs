d1a4fed42e3a7cc167a06a1284cbaa759916b4bd
vladimir-koloski <v.koloski1@gmail.com>
2020-04-05 12:45:33 +0200

    change

--- Homework01/SEDC.Class06.Exercises/SEDC.Class06.Exercises.App/Program.cs
+++ Homework01/SEDC.Class06.Exercises/SEDC.Class06.Exercises.App/Program.cs
@@ -8,88 +8,93 @@ namespace SEDC.Class06.Exercises.App
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 'Register' if you want to make new profile or 'Login' if you are allready registred");
+            if (Console.ReadLine() == "Register")
+            {
+            Console.WriteLine("Enter email:");
+            string inputEmail = Console.ReadLine();
+            EnterEmail(inputEmail);
+            Console.WriteLine("Enter password:");
+            string inputPasword = Console.ReadLine();
+            EnterPassword(inputPasword);
+            Console.WriteLine("Enter First Name:");
+            string inputFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name:");
+            string inputLastName = Console.ReadLine();
+            Console.WriteLine("Enter Date of Birth:");
+            DateTime inpututDate = Convert.ToDateTime(Console.ReadLine());
             User[] users = new User[] { };
             User user = new User(inputEmail, inputPasword, inputFirstName, inputLastName, inpututDate);
-            Array.Resize(ref users, users.Length + 1);
-            users[users.Length - 1] = user;
+            Register(User[] users, user);
+                   
+            }
+            //Console.WriteLine("Do you want to register another user? Enter yes or no");
+            //string action = Console.ReadLine();
 
-            Console.ReadLine();
+            //if (action == "yes")
+            //{
+            //    Register();
+            //}
+            //else
+            //{
+            //    registration = false;
+            //}
+            //if (Console.ReadLine() == "Login")
+            {
+                Console.WriteLine("Enter email:");
+                string inputEmail = Console.ReadLine();                
+                Console.WriteLine("Enter password:");
+                string inputPasword = Console.ReadLine();
+                if(Login(inputEmail, inputPasword) == true)
+                {
+                    PrintUsers(User[] users);
+                }              
+                
+                
+            }
+
+                Console.ReadLine();
         }
 
-        public static void Register(User[] users) samo da mu dadam niza i juzer i da go registrtiram userot, i register i logn da mi vratat triue alsem i nakraj printaj useri so foreach
-            login  emai i password i lista od useri, izvrti ako se sovpagjaat true i isprintaj site useri
-
-            Print all users
+        public void PrintUsers(User [] users)
         {
-            bool registration = true;
-            while (registration)
+            foreach (var user in users)
             {
-                Console.WriteLine("Enter 'Register' if you want to make new profile or 'Login' if you are allready registred");
-                if (Console.ReadLine() == "Register")
-                {
-                    while (registration)
-                    {
-                        Console.WriteLine("Enter email:");
-                        string inputEmail = Console.ReadLine();
-                        EnterEmail(inputEmail);
-                        Console.WriteLine("Enter password:");
-                        string inputPasword = Console.ReadLine();
-                        EnterPassword(inputPasword);
-                        Console.WriteLine("Enter First Name:");
-                        string inputFirstName = Console.ReadLine();
-                        Console.WriteLine("Enter Last Name:");
-                        string inputLastName = Console.ReadLine();
-                        Console.WriteLine("Enter Date of Birth:");
-                        DateTime inpututDate = Convert.ToDateTime(Console.ReadLine());
-
-
-                        
-                        foreach (var user1 in users)
-                        {
-                            if (inputEmail == user1.Email && inputPasword == user1.Password)
-                                Console.WriteLine(user.GetInfo());
-                        }
-                        Console.WriteLine("Do you want to register another user? Enter yes or no");
-                        string action = Console.ReadLine();
-
-
-                        if (action == "yes")
-                        {
-                            Register();
-                        }
-                        else
-                        {
-                            registration = false;
-                        }
-
-                    }
-                }
-
-                if (Console.ReadLine() == "Login")
-                {
-                    Console.WriteLine("Enter email:");
-                    string inputEmail = Console.ReadLine();
-                    Console.WriteLine("Enter password:");
-                    string inputPasword = Console.ReadLine();
-                    foreach (var user in users)
-                    {
-                        if (inputEmail == user.Email && inputPasword == user.Password)
-                            Console.WriteLine(user.GetInfo());
-                    }
-                    registration = false;
-                }
-
+               Console.WriteLine(user.GetInfo());
             }
-            static string EnterEmail(string email)
+        }
+
+        
+        public static void Register(User[] users, User user)            
+        {      
+        
+            {
+                    Array.Resize(ref users, users.Length + 1);
+                    users[users.Length - 1] = user;               
+            }
+        }
+        
+        public bool Login(string email, string password)
+            {
+                foreach (var user in users)
+                {
+                    if (email == user.Email && password == user.Password);
+                       
+                }
+                return true;
+                
+            }
+
+             public static void EnterEmail(string email)
             {
                 var validationService = new ValidationService();
                 if (!validationService.ValidateEmail(email))
                 {
                     Console.WriteLine("Enter Valid email");
                 }
-                return email;
+
             }
-            static string EnterPassword(string password)
+            public static string EnterPassword(string password)
             {
                 var validationService = new ValidationService();
                 if (!validationService.ValidatePassword(password))
@@ -104,7 +109,7 @@ namespace SEDC.Class06.Exercises.App
 
 
     }
-}
+
 
 
  
